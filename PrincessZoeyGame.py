1d54c012c7c6f85f23f8cef2169d6f08ca4e7d36
michael <mreece524@gmail.com>
2017-01-29 00:03:18 -0600

    refactor

--- PrincessZoeyGame.py
+++ PrincessZoeyGame.py
@@ -3,6 +3,10 @@ import pygame
 import pytmx
 import os
 
+#initiate pygame
+pygame.init()
+game_display = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("Princess Zoey's Adventure")
 
 class Level(object):
             
@@ -28,27 +32,48 @@ class Level(object):
         self.render(temp_surface)
         return temp_surface
 
-#initiate pygame
-pygame.init()
-game_display = pygame.display.set_mode((1024, 768))
-pygame.display.set_caption("Princess Zoey's Adventure")
-fps_clock = pygame.time.Clock()
-level = Level("/assets/level/test_level.tmx")
-tileSurface = level.make_map()
+class ZoeyGame(object):
 
+    def __init__(self, game_display):
+        self.fps_clock = pygame.time.Clock()
+        self.gameOver = False
+        self.x = 0
+        self.currentHandler = ZoeyGameEventHandler.GamePlayEventHandler()
+        self.game_display = game_display
+        self.loadLevel()
+        self.gameLoop()
 
-gameOver = False
+    def loadLevel(self):
+        level = Level("/assets/level/test_level.tmx")
+        self.tileSurface = level.make_map()  
 
-currentHandler = ZoeyGameEventHandler.GamePlayEventHandler()
+    def endGame(self, bool_endGame):
+        self.gameOver = endGame   
 
-def endGame(bool_endGame):
-    gameOver = endGame
+    def gameLoop(self):
+        xpos = 0;
+        while not self.gameOver:
+            xpos += self.x
+            self.game_display.blit(self.tileSurface, (0 + xpos, 0))
+            pygame.display.update()
+            self.handleEvent(pygame.event)
+            if(self.currentHandler.isEndGame()):
+                self.gameOver = True
+        self.fps_clock.tick(30)
+        
+    def handleEvent(self, pygame_event):    
+        for event in pygame_event.get():
+            if event.type == pygame.QUIT:
+                self.gameOver = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.x = -5
+                elif event.key == pygame.K_LEFT:
+                    self.x = +5
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.x = 0
+                elif event.key == pygame.K_LEFT:
+                    self.x = 0
 
-
-while not gameOver:
-    game_display.fill((0, 100, 200))
-    game_display.blit(tileSurface, (0, 0))
-    pygame.display.update()
-    currentHandler.handleEvent(pygame.event)
-    if(currentHandler.isEndGame()):
-        gameOver = True
+ZoeyGame(game_display)
