e3f6e36fe3045325e86fc4912decb648578ee75f
Michael Reece <Michael Reece>
2017-02-02 22:14:38 -0600

    added zombie animation, player direction

--- ZoeyGameSprites.py
+++ ZoeyGameSprites.py
@@ -6,9 +6,11 @@ class PrincessSprite(pygame.sprite.Sprite):
 
     def __init__(self, start_coord):
         pygame.sprite.Sprite.__init__(self)
-        self.MOVE_SPEED = 3.5
+        self.MOVE_SPEED = 2
         self.space_pushed = False
-        self.image = pygame.image.load("./assets/art/zoeyPlaceHolder.png")
+        self.image_right = pygame.image.load("./assets/art/zoeyPlaceHolder.png")
+        self.image_left = pygame.transform.flip(self.image_right, True, False)
+        self.image = self.image_right
         self.rect = self.image.get_rect()
         self.rect.inflate_ip(-20, 0)
         self.vel = vec(0, 0)
@@ -28,17 +30,19 @@ class PrincessSprite(pygame.sprite.Sprite):
         self.acc.y = -15
 
     def update(self, friction, gravity):
-        print("vel = "+str(self.vel) + "acc = "+str(self.acc) + "pos = "+str(self.pos))
+        #print("vel = "+str(self.vel) + "acc = "+str(self.acc) + "pos = "+str(self.pos))
         self.acc = vec(0, gravity)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.acc.x = -self.MOVE_SPEED
             if not self.onGround and self.vel.x > .05:
                 self.acc.x = -self.MOVE_SPEED/2
+            self.image = self.image_left
         if keys[pygame.K_RIGHT]:
             self.acc.x = self.MOVE_SPEED
             if not self.onGround and self.vel.x < -.05:
                 self.acc.x = self.MOVE_SPEED/2
+            self.image = self.image_right
         if keys[pygame.K_SPACE]:
             if self.onGround  and not self.space_pushed:
                 self.space_pushed = True
@@ -68,10 +72,11 @@ class PrincessSprite(pygame.sprite.Sprite):
             self.pos.y = object.rect.top-self.rect.height
 
     def kill_enemy(self, enemy, gravity):
-        y_vel = self.vel.y + .5 * self.acc.y
+        y_vel = math.ceil(self.vel.y + .5 * self.acc.y)
+      #  print('enemy top: '+str(enemy.rect.top) + ' self bottom: '+str(self.rect.bottom) + ' yvel = '+str(y_vel)
+       # +' vel = '+str(self.vel))
         if self.vel.y - gravity > 0 and enemy.rect.top >= self.rect.bottom - y_vel:
             self.vel.y = -8
-            #self.vel.y += -25
             return True
         else:
             return False
@@ -88,7 +93,16 @@ class obstacle(pygame.sprite.Sprite):
 class snake(pygame.sprite.Sprite):
     def __init__(self, x, y, travel = 40):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("./assets/art/yellow_snake.png")
+        self.UPDATE_FRAME_ON = 150
+        self.current_frame = 0
+        self.last_frame = 0
+        self.TRAVEL_SPEED = 1
+        self.sprite_width = 52
+        self.sprite_height = 87
+        self.spritesheet = Spritesheet("./assets/art/zombie_sprite_sheet.png")
+        self.load_images()
+        self.image = self.walking_frames_left[0]
+        self.image_set = self.walking_frames_left
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -96,23 +110,72 @@ class snake(pygame.sprite.Sprite):
         self.x_max_travel = travel
         self.vel = vec(0, 0)
         self.pos = vec(x, y)
-        self.acc = vec(2,0)
+        self.acc = vec(-self.TRAVEL_SPEED,0)
+
+    def load_images(self):
+        self.walking_frames_left = [self.spritesheet.get_image_row_column(self.sprite_width,self.sprite_height,2,0),
+                               self.spritesheet.get_image_row_column(self.sprite_width,self.sprite_height,1,0),
+                               self.spritesheet.get_image_row_column(self.sprite_width,self.sprite_height,0,0),
+                               self.spritesheet.get_image_row_column(self.sprite_width,self.sprite_height,2,0),
+                               self.spritesheet.get_image_row_column(self.sprite_width,self.sprite_height,3,0),
+                               self.spritesheet.get_image_row_column(self.sprite_width,self.sprite_height,4,0)]
+
+        self.walking_frames_right = []
+        for frame in self.walking_frames_left:
+            self.walking_frames_right.append(pygame.transform.flip(frame,True, False))
+
 
     def update(self, friction, gravity):
-        if math.fabs(self.x_travel) < self.x_max_travel:
+        force = False
+        if math.fabs(self.x_travel) <= self.x_max_travel:
             self.rect.x += self.acc.x
             self.x_travel += self.acc.x
         else:
             self.x_travel = 0
             if self.acc.x > 0:
-                self.acc.x = -2
+                self.acc.x = -self.TRAVEL_SPEED
+                self.current_frame = 0
+                self.image_set = self.walking_frames_left
+                force = True
             else:
-                self.acc.x = 2        
+                self.acc.x = self.TRAVEL_SPEED 
+                self.current_frame = 0
+                self.image_set = self.walking_frames_right
+                force = True
+        self.animate(force)
+
+    def animate(self, force):
+        now = pygame.time.get_ticks()
+        print('now : '+str(now) + ' last_frame: '+str(self.last_frame))
+        if (now - self.last_frame >= self.UPDATE_FRAME_ON) or (force):
+            self.last_frame = now
+            self.current_frame += 1
+            if self.current_frame >= len(self.walking_frames_left):
+               self.current_frame = 0
+            self.image = self.image_set[self.current_frame]
 
 class eye(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("./assets/art/shining_eye.png")
+        self.image = pygame.image.load("./assets/art/peeper_tall.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+class Spritesheet:
+        def __init__(self, filename):
+            self.COLOR_KEY = (255,20,147) #PINK
+            self.spritesheet = pygame.image.load(filename).convert()
+
+        def get_image(self, x, y, width, height):
+            
+            image = pygame.Surface((width, height))
+            image.set_colorkey(self.COLOR_KEY)
+            image.blit(self.spritesheet, (0, 0), (x,y,width,height))
+            return image
+        
+        def get_image_row_column(self,width,height,column, row):
+            x = width * column
+            y = height * row
+            #print('loading image at x:'+ str(x) +" y: " +str(y))
+            return self.get_image(x, y, width, height)
